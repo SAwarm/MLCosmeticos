77118e2a6139dfbf00c3a74c5734e64edb1111dd
SAwarm <jonasdasilvaelias@gmail.com>
2021-11-17 13:46:59 -0300

    backend insert produto

--- admin/backend/cadastro_produto_dimensao.php
+++ admin/backend/cadastro_produto_dimensao.php
@@ -1,4 +1,55 @@
 <?php 
 
     require_once('connection.php');
-    print_r($_POST);
+
+    $largura = $_POST['largura'];
+    $altura = $_POST['altura'];
+    $peso = $_POST['peso'];
+    $profundidade = $_POST['profundidade'];
+    $obs_dimensao = $_POST['observacao_dimensao'];
+
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    $imagem = $_POST['imagem'];
+    $cor = $_POST['cor'];
+    $marca = $_POST['marca'];
+    $obs_produto = $_POST['observacao'];
+    $tipo = $_POST['tipo'];
+    
+    function insertDimensao($conn, $largura, $altura, $peso, $profundidade, $obs_dimensao){
+        $sql = "INSERT INTO dimensoes (altura, largura, peso, profundidade, obs) VALUES ('$altura', '$largura', '$peso', '$profundidade', '$obs_dimensao')";
+
+        mysqli_query($conn, $sql);
+        return mysqli_insert_id($conn);
+    }
+
+    $id_dimensao = insertDimensao($connection, $largura, $altura, $peso, $profundidade, $obs);
+
+    function insertProduto($conn, $nome, $descricao, $preco, $imagem, $cor, $marca, $dimensoes_id, $obs_produto, $tipo){
+        $sql = "INSERT INTO produto (
+            nome,
+            descricao,
+            preco,
+            imagem,
+            cor,
+            marca,
+            dimensoes_cod,
+            obs,
+            tipo
+        ) VALUES (
+            '$nome',
+            '$descricao',
+            '$preco',
+            '$imagem',
+            '$cor',
+            '$marca',
+            '$dimensoes_id',
+            '$obs',
+            '$tipo'
+        )";
+
+        mysqli_query($conn, $sql);
+        return mysqli_insert_id($conn);
+    }
+   
--- admin/produtos.php
+++ admin/produtos.php
@@ -28,62 +28,18 @@
                                     Adicionar Produto</a>
                     <div class="col-sm-12">
                         <div class="white-box">
-                            <h3 class="box-title">Basic Table</h3>
-                            <p class="text-muted">Add class <code>.table</code></p>
+                            <h3 class="box-title">Produtos</h3>
                             <div class="table-responsive">
                                 <table class="table text-nowrap">
                                     <thead>
                                         <tr>
-                                            <th class="border-top-0">#</th>
-                                            <th class="border-top-0">First Name</th>
-                                            <th class="border-top-0">Last Name</th>
-                                            <th class="border-top-0">Username</th>
-                                            <th class="border-top-0">Role</th>
+                                            <th class="border-top-0">Id</th>
+                                            <th class="border-top-0">Descrição</th>
+                                            <th class="border-top-0">Preço</th>
+                                            <th class="border-top-0">Opções</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Deshmukh</td>
-                                            <td>Prohaska</td>
-                                            <td>@Genelia</td>
-                                            <td>admin</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Deshmukh</td>
-                                            <td>Gaylord</td>
-                                            <td>@Ritesh</td>
-                                            <td>member</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Sanghani</td>
-                                            <td>Gusikowski</td>
-                                            <td>@Govinda</td>
-                                            <td>developer</td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td>Roshan</td>
-                                            <td>Rogahn</td>
-                                            <td>@Hritik</td>
-                                            <td>supporter</td>
-                                        </tr>
-                                        <tr>
-                                            <td>5</td>
-                                            <td>Joshi</td>
-                                            <td>Hickle</td>
-                                            <td>@Maruti</td>
-                                            <td>member</td>
-                                        </tr>
-                                        <tr>
-                                            <td>6</td>
-                                            <td>Nigam</td>
-                                            <td>Eichmann</td>
-                                            <td>@Sonu</td>
-                                            <td>supporter</td>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
@@ -271,6 +227,10 @@
     $(".dimensoes").click(function(e){
         $('#modal-dimensoes').modal('show');
     })
+
+    function loadTable(){
+        
+    }
 
     $(".btn-add-produto").click(function(e){
         nome = $('.nome').val();
